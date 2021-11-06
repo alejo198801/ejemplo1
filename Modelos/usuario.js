f72c3c3b207fbdb77c8f33e844cd24f7f557d5d3
Alejandro <alcastano001@gmail.com>
2021-11-06 08:54:12 -0500

    guardado

--- Modelos/usuario.js
+++ Modelos/usuario.js
@@ -1,9 +1,15 @@
 const mongoose= require("mongoose");
 let usuarioSchema = new mongoose.Schema({
+    tipo_documento : String,
     idusuario : Number,
     nombreusuario : String,
-    telefonousuario : Number,
-    ciudadusuario : String
+    apellidousuario: String,
+    direccion_usuario: String,
+    correo_usuario: String,
+    telefono_usuario : Number,
+    celular_usuario: String,
+    web_usuario: String,
+    perfil_usuario: String,
 });
 
 module.exports = mongoose.model("usuario", usuarioSchema, "usuarios");
